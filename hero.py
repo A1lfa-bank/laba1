d1ed3ae9344737d4d82494db37205fd831311dfd
A1lfa-bank <152848615+A1lfa-bank@users.noreply.github.com>
2024-09-10 18:28:29 +0300

    hero.py

--- hero.py
+++ hero.py
@@ -2,7 +2,6 @@
 # license: GPLv3
 from gameunit import *
 
-#FIXME:
 """В этом файле должен быть описан класс героя, унаследованный от Attacker
 Герой должен иметь 100 поинтов здоровья, атаку 50, опыт 0, имя, задаваемое в конструкторе
 Метод attack должен получать атрибут target и уменьшать его здоровье на величину атаки.
@@ -14,5 +13,6 @@ class Hero(Attacker):
     _health = 100
     def __init__(self, name):
         name = None
+
     def attack(self, target):
         target._health -= self._attack
